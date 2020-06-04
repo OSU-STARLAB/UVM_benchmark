d715602822fa7784e395113a297dd4e2e13e1ea0
guyo <guyongbin1325@gmail.com>
2020-06-03 21:05:58 -0700

    fix bugs of memory occupation

--- UVM_benchmarks_oversub/Memory_occupy/memory_occ.cu
+++ UVM_benchmarks_oversub/Memory_occupy/memory_occ.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
+#include<stdlib.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <unistd.h>
@@ -13,18 +14,25 @@
 int main(int argc, char *argv[])
 {
     unsigned GB,MB,KB;
+    char *endptr;
 
     fprintf(stdout, "Input %d argument\n",argc);
     assert(argc == 3);
-    int size_char_format = atoi(argv[1]);
+    // float size_char_format = atoi(argv[1]);
+    float size = strtof(argv[1], &endptr);
+    if (*endptr != '\0')
+        printf("%s is not convertible to float\n", argv[1]);
+    else
+        printf("size is  %f\n", size ); //0.000000    
     int over_sub_char_format = atoi(argv[2]);
-    float size = (float) size_char_format;
+    // float size = (float) size_char_format;
     unsigned over_sub = (unsigned) over_sub_char_format;
 
     if (over_sub == 1)
     {
+        // printf("size is %f\n", size);
         fprintf(stdout, "It is 110%% oversubscription mode\n");
-        float occupy_mem = 11  - size * 10.0/11.0;
+        float occupy_mem = 11  - size   +  size * 1.0/10.0 - 0.1;
         GB = (unsigned) occupy_mem;
         float temp_MB = (occupy_mem -floor(occupy_mem)) *1024.0;
         MB = (unsigned) temp_MB;
@@ -33,8 +41,9 @@ int main(int argc, char *argv[])
     }
     else if (over_sub == 2)
     {
+        printf("size is %f\n", size);
         fprintf(stdout, "It is 125%% oversubscription mode\n");
-        float occupy_mem = 11  - size * 10.0/12.5;
+        float occupy_mem = 11  - size   +  size * 1.0/4.0 - 0.1; 
         GB = (unsigned) occupy_mem;
         float temp_MB = (occupy_mem -floor(occupy_mem)) *1024.0;
         MB = (unsigned) temp_MB;
@@ -68,7 +77,11 @@ int main(int argc, char *argv[])
     cudaMalloc((void **) &MB_ptr, sizeof(float) * 1024*256 * MB);
     cudaMalloc((void **) &KB_ptr, sizeof(float) * 256 * KB);
     
-    sleep(100);
+    sleep(900);
+    sleep(900);
+    sleep(900);
+    sleep(900);
+    sleep(900);
     
 
     cudaFree(GB_ptr);
